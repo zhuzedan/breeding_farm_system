5ee1b66f44697b5514ed481129f011ec989d1074
zhuzedan <1031155817@qq.com>
2023-05-04 09:22:02 +0800

    init

---
+++ src/main/java/com/common/dao/YzcBornRecordMapper.java
@@ -0,0 +1,14 @@
+package com.common.dao;
+
+import com.common.entity.YzcBornRecord;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+  * 出生记录表 Mapper 接口
+ * </p>
+ * @since 2017-04-16
+ */
+public interface YzcBornRecordMapper extends BaseMapper<YzcBornRecord> {
+
+}
